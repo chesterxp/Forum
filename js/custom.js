d1ddfe5d287123382598c019c91a56e864df5413
chesterxp <chesterxp7@wp.pl>
2017-02-27 23:03:52 +0100

    gallery

--- js/custom.js
+++ js/custom.js
@@ -20,73 +20,145 @@ $(document).ready(function(){
 		cssSelectorAncestor: "#jp_container_1"
 	}, [
 		{
-			title:"ona tańczy dla mnie",
+			title:"Kochać Cię za późno - z repertuaru zespołu Kombi",
 			mp3:"audio/Track%2003.mp3",
 			oga:"audio/Track%2003.mp3"
 		},
 		{
-			title:"Your Face",
+			title:"Move in the Right Direction - z repertuaru zespołu Gossip",
 			mp3:"http://www.jplayer.org/audio/mp3/TSP-05-Your_face.mp3",
 			oga:"http://www.jplayer.org/audio/ogg/TSP-05-Your_face.ogg"
 		},
 		{
-			title:"Cyber Sonnet",
-			mp3:"http://www.jplayer.org/audio/mp3/TSP-07-Cybersonnet.mp3",
-			oga:"http://www.jplayer.org/audio/ogg/TSP-07-Cybersonnet.ogg"
+			title:"Ona i on - z repertuaru Weekend",
+			mp3:"audio/Track%2003.mp3",
+			oga:"audio/Track%2003.mp3"
 		},
 		{
-			title:"Tempered Song",
+			title:"Za daleko mieszkasz miły - z repertuaru A. Rusowicz",
 			mp3:"http://www.jplayer.org/audio/mp3/Miaow-01-Tempered-song.mp3",
 			oga:"http://www.jplayer.org/audio/ogg/Miaow-01-Tempered-song.ogg"
 		},
 		{
-			title:"Hidden",
+			title:"Oj Smereko - piosenka ukraińska",
 			mp3:"http://www.jplayer.org/audio/mp3/Miaow-02-Hidden.mp3",
 			oga:"http://www.jplayer.org/audio/ogg/Miaow-02-Hidden.ogg"
 		},
 		{
-			title:"Lentement",
-			free:true,
+			title:"Co za noc - z repertuaru Dystans",
 			mp3:"http://www.jplayer.org/audio/mp3/Miaow-03-Lentement.mp3",
 			oga:"http://www.jplayer.org/audio/ogg/Miaow-03-Lentement.ogg"
 		},
 		{
-			title:"Lismore",
+			title:"Statki na niebie - z repertuaru De Mono",
 			mp3:"http://www.jplayer.org/audio/mp3/Miaow-04-Lismore.mp3",
 			oga:"http://www.jplayer.org/audio/ogg/Miaow-04-Lismore.ogg"
 		},
 		{
-			title:"The Separation",
+			title:"Tell Me Why - z repertuaru Amna",
 			mp3:"http://www.jplayer.org/audio/mp3/Miaow-05-The-separation.mp3",
 			oga:"http://www.jplayer.org/audio/ogg/Miaow-05-The-separation.ogg"
 		},
 		{
-			title:"Beside Me",
+			title:"Szukam dziewczyny - z repertuaru Masters",
 			mp3:"http://www.jplayer.org/audio/mp3/Miaow-06-Beside-me.mp3",
 			oga:"http://www.jplayer.org/audio/ogg/Miaow-06-Beside-me.ogg"
 		},
 		{
-			title:"Bubble",
-			free:true,
+			title:"Simply the best  - z repertuaru Tiny Turner",
 			mp3:"http://www.jplayer.org/audio/mp3/Miaow-07-Bubble.mp3",
 			oga:"http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
 		},
 		{
-			title:"Stirring of a Fool",
+			title:"Jesteś częścią mnie - z repertuaru For Teens",
 			mp3:"http://www.jplayer.org/audio/mp3/Miaow-08-Stirring-of-a-fool.mp3",
 			oga:"http://www.jplayer.org/audio/ogg/Miaow-08-Stirring-of-a-fool.ogg"
 		},
 		{
-			title:"Partir",
-			free: true,
+			title:"Wielka dama - z repertuaru Anny Jantar",
 			mp3:"http://www.jplayer.org/audio/mp3/Miaow-09-Partir.mp3",
 			oga:"http://www.jplayer.org/audio/ogg/Miaow-09-Partir.ogg"
 		},
 		{
-			title:"Thin Ice",
+			title:"Prawdziwe powietrze - z repertuaru Loka",
+			mp3:"http://www.jplayer.org/audio/mp3/Miaow-10-Thin-ice.mp3",
+			oga:"http://www.jplayer.org/audio/ogg/Miaow-10-Thin-ice.ogg"
+		},
+		
+		{
+			title:"Touch me - z repertuaru Samanthy Fox",
+			mp3:"http://www.jplayer.org/audio/mp3/Miaow-10-Thin-ice.mp3",
+			oga:"http://www.jplayer.org/audio/ogg/Miaow-10-Thin-ice.ogg"
+		},
+		
+		{
+			title:"Ruchome piaski - z repertuaru Varius Manx",
+			mp3:"http://www.jplayer.org/audio/mp3/Miaow-10-Thin-ice.mp3",
+			oga:"http://www.jplayer.org/audio/ogg/Miaow-10-Thin-ice.ogg"
+		},
+		{
+			title:"Nogi - z repertuaru Czarno Czarni",
+			mp3:"http://www.jplayer.org/audio/mp3/Miaow-10-Thin-ice.mp3",
+			oga:"http://www.jplayer.org/audio/ogg/Miaow-10-Thin-ice.ogg"
+		},
+		{
+			title:"Kolorowe jarmarki - wersja Maryli Rodowicz",
+			mp3:"http://www.jplayer.org/audio/mp3/Miaow-10-Thin-ice.mp3",
+			oga:"http://www.jplayer.org/audio/ogg/Miaow-10-Thin-ice.ogg"
+		},
+		{
+			title:"Zanim wstanie dzień - z repertuaru Adama",
+			mp3:"http://www.jplayer.org/audio/mp3/Miaow-10-Thin-ice.mp3",
+			oga:"http://www.jplayer.org/audio/ogg/Miaow-10-Thin-ice.ogg"
+		},
+		{
+			title:"Trzynastego - z repertuaru Kasi Sobczyk ",
+			mp3:"http://www.jplayer.org/audio/mp3/Miaow-10-Thin-ice.mp3",
+			oga:"http://www.jplayer.org/audio/ogg/Miaow-10-Thin-ice.ogg"
+		},
+		{
+			title:"Promienie - z repertuaru Tarzan Boy",
+			mp3:"http://www.jplayer.org/audio/mp3/Miaow-10-Thin-ice.mp3",
+			oga:"http://www.jplayer.org/audio/ogg/Miaow-10-Thin-ice.ogg"
+		},
+		{
+			title:"Marysiu buzi daj - z repertuaru ludowego",
+			mp3:"http://www.jplayer.org/audio/mp3/Miaow-10-Thin-ice.mp3",
+			oga:"http://www.jplayer.org/audio/ogg/Miaow-10-Thin-ice.ogg"
+		},
+		{
+			title:"Słowo jedyne Ty - z repertuaru Czerwone Gitary ",
+			mp3:"http://www.jplayer.org/audio/mp3/Miaow-10-Thin-ice.mp3",
+			oga:"http://www.jplayer.org/audio/ogg/Miaow-10-Thin-ice.ogg"
+		},
+		{
+			title:"Małgośka - z repertuaru Maryli Rodowicz",
+			mp3:"http://www.jplayer.org/audio/mp3/Miaow-10-Thin-ice.mp3",
+			oga:"http://www.jplayer.org/audio/ogg/Miaow-10-Thin-ice.ogg"
+		},
+		{
+			title:"Na falochronie - z repertuaru Emigranci",
+			mp3:"http://www.jplayer.org/audio/mp3/Miaow-10-Thin-ice.mp3",
+			oga:"http://www.jplayer.org/audio/ogg/Miaow-10-Thin-ice.ogg"
+		},
+		{
+			title:"Weselne dzieci - z repertuaru Maryli Rodowicz ",
+			mp3:"http://www.jplayer.org/audio/mp3/Miaow-10-Thin-ice.mp3",
+			oga:"http://www.jplayer.org/audio/ogg/Miaow-10-Thin-ice.ogg"
+		},
+		{
+			title:"Moja Gitara - z repertuaru Lider",
+			mp3:"http://www.jplayer.org/audio/mp3/Miaow-10-Thin-ice.mp3",
+			oga:"http://www.jplayer.org/audio/ogg/Miaow-10-Thin-ice.ogg"
+		},
+		{
+			title:"Dla zakochanych - z repertuaru De Mono",
 			mp3:"http://www.jplayer.org/audio/mp3/Miaow-10-Thin-ice.mp3",
 			oga:"http://www.jplayer.org/audio/ogg/Miaow-10-Thin-ice.ogg"
 		}
+		
+		
+		
 	], {
 		swfPath: "../../dist/jplayer",
 		supplied: "oga, mp3",
